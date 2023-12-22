8deaacd6baa5ca3c6ecddca0e7eaf1fd674391bc
Josman Santiago Muñoz Mera <josmansant@unicauca.edu.co>
2023-12-21 22:09:09 -0500

    Maquetado

--- src/components/courseManagementTable/courseManagementTable.jsx
+++ src/components/courseManagementTable/courseManagementTable.jsx
@@ -1,90 +1,111 @@
-import Table from 'react-bootstrap/Table';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styled from 'styled-components';
+import "bootstrap/dist/css/bootstrap.min.css";
+import styled from "styled-components";
 
 const EditButton = styled.button`
-    color: #fff;
-    background-color: #34A853;
-`
+  color: #fff;
+  background-color: #34a853;
+`;
 const DeleteButton = styled.button`
-    color: #fff;
-    background-color: #C50B0A;
-`
+  color: #fff;
+  background-color: #c50b0a;
+`;
 const TdButtons = styled.td`
-    display: flex;
+  display: flex;
+`;
+const Table = styled.table`
+  border-collapse: collapse;
+  tr,th,td {
+    background-color: #ECECEC;
+    border: none;
+  }
+`;
 
-`
+// Define los estilos para filas pares
+const EvenRow = styled.tr`
+  background-color: #ECECEC;
+`;
+
+// Define los estilos para filas impares
+const OddRow = styled.tr`
+  background-color: #282829;
+  td{
+    background-color:#ECECEC;
+  }
+`;
 function CourseManagementTable() {
   const columns = [
     {
-      Header: 'No.',
-      accessor: 'No.',
+      Header: "No.",
+      accessor: "No.",
     },
     {
-      Header: 'Nombre',
-      accessor: 'name',
+      Header: "Nombre",
+      accessor: "name",
     },
     {
-      Header: 'Descripción',
-      accessor: 'description',
+      Header: "Descripción",
+      accessor: "description",
     },
     {
-      Header: 'Categoría',
-      accessor: 'category',
+      Header: "Categoría",
+      accessor: "category",
     },
     {
-      Header: 'Intensidad',
-      accessor: 'intensity',
+      Header: "Intensidad",
+      accessor: "intensity",
     },
     {
-      Header: 'Fecha Inicio',
-      accessor: 'startDate',
+      Header: "Fecha Inicio",
+      accessor: "startDate",
     },
     {
-      Header: 'Fecha Final',
-      accessor: 'endDate',
+      Header: "Fecha Final",
+      accessor: "endDate",
     },
     {
-      Header: 'Resolucion',
-      accessor: 'resolution',
+      Header: "Resolucion",
+      accessor: "resolution",
     },
     {
-      Header: 'Costo',
-      accessor: 'cost',
+      Header: "Costo",
+      accessor: "cost",
     },
     {
-      Header: 'Accón',
-      accessor: 'action',
+      Header: "Accón",
+      accessor: "action",
     },
-  ]
+  ];
   const data = [
     {
-      numero :'1',
-      nombre :'Diplomado en Rehabilitación Vestibular',
-      descripcion :'El diplomado en Rehabilitación Vestibular es un programa de formación que busca desarrollar competencias en el profesional de la salud para la evaluación y tratamiento de pacientes con alteraciones vestibulares.',
-      categoria :'Salud',
-      intensidad :'120 horas',
-      fechaInicio :'2021-09-01',
-      fechaFinal :'2021-12-01',
-      resolucion :'12345',
-      costo :'200000',
-      accion :'Editar',
+      numero: "1",
+      nombre: "Diplomado en Rehabilitación Vestibular",
+      descripcion:
+        "El diplomado en Rehabilitación Vestibular es un programa de formación que busca desarrollar competencias en el profesional de la salud para la evaluación y tratamiento de pacientes con alteraciones vestibulares.",
+      categoria: "Salud",
+      intensidad: "120 horas",
+      fechaInicio: "2021-09-01",
+      fechaFinal: "2021-12-01",
+      resolucion: "12345",
+      costo: "200000",
+      accion: "Editar",
     },
     {
-      numero :'2',
-      nombre :'Diplomado en Medicina tradicional china con énfasis en acupuntura',
-      descripcion :'El diplomado en Medicina tradicional china con énfasis en acupuntura es un programa de formación que busca desarrollar competencias en el profesional de la salud para la evaluación y tratamiento de pacientes con alteraciones vestibulares.',
-      categoria :'Salud',
-      intensidad :'120 horas',
-      fechaInicio :'2021-09-01',
-      fechaFinal :'2021-12-01',
-      resolucion :'12345',
-      costo :'200000',
-      accion :'Editar'
-    }
-  ]
+      numero: "2",
+      nombre:
+        "Diplomado en Medicina tradicional china con énfasis en acupuntura",
+      descripcion:
+        "El diplomado en Medicina tradicional china con énfasis en acupuntura es un programa de formación que busca desarrollar competencias en el profesional de la salud para la evaluación y tratamiento de pacientes con alteraciones vestibulares.",
+      categoria: "Salud",
+      intensidad: "120 horas",
+      fechaInicio: "2021-09-01",
+      fechaFinal: "2021-12-01",
+      resolucion: "12345",
+      costo: "200000",
+      accion: "Editar",
+    },
+  ];
   return (
-    <div  className="table-responsive">
+    <div className="table-responsive">
       <Table className="table table-bordered table-hover">
         <thead>
           <tr>
@@ -94,9 +115,9 @@ function CourseManagementTable() {
           </tr>
         </thead>
         <tbody>
-          {
-            data.map((row,index) => (
-              <tr key={index}>
+          {data.map((row, index) =>
+            index % 2 === 0 ? (
+              <EvenRow key={index}>
                 <td>{row.numero}</td>
                 <td>{row.nombre}</td>
                 <td>{row.descripcion}</td>
@@ -106,15 +127,33 @@ function CourseManagementTable() {
                 <td>{row.fechaFinal}</td>
                 <td>{row.resolucion}</td>
                 <td>{row.costo}</td>
-                <TdButtons><EditButton>Editar</EditButton> <DeleteButton>Eliminar</DeleteButton></TdButtons>
-              </tr>
-            ))
-          }
+                <TdButtons>
+                  <EditButton>Editar</EditButton>{" "}
+                  <DeleteButton>Eliminar</DeleteButton>
+                </TdButtons>
+              </EvenRow>
+            ) : (
+              <OddRow key={index}>
+                <td>{row.numero}</td>
+                <td>{row.nombre}</td>
+                <td>{row.descripcion}</td>
+                <td>{row.categoria}</td>
+                <td>{row.intensidad}</td>
+                <td>{row.fechaInicio}</td>
+                <td>{row.fechaFinal}</td>
+                <td>{row.resolucion}</td>
+                <td>{row.costo}</td>
+                <TdButtons>
+                  <EditButton>Editar</EditButton>{" "}
+                  <DeleteButton>Eliminar</DeleteButton>
+                </TdButtons>
+              </OddRow>
+            )
+          )}
         </tbody>
-
       </Table>
     </div>
-  )
+  );
 }
 
-export default CourseManagementTable
+export default CourseManagementTable;
