593521ef9e47881c542c73c173913404e2696d14
jeco880522 <juan.contreras.ospina@unillanos.edu.co>
2023-12-26 13:45:04 -0500

    add responsive recuperacion contraseña

--- src/components/Recuperacion/Recuperacion.jsx
+++ src/components/Recuperacion/Recuperacion.jsx
@@ -2,25 +2,29 @@ import React from 'react';
 import logo from "../Recuperacion/logodemoday.png";
 import './stylerecu.css'
 
+
 const Recuperacion = () => {
-  return (    
-    <div className="container" id="container">
-      <div className="form-container sign-in-container">
-        <form action="#">
-          <h3>RECUPERAR CONTRASEÑA</h3>
-          <span>Ingresa tu correo electrónico para buscar tu cuenta.</span> 
-          <input type="email" placeholder="Correo Electronico" />
-          <button>Continuar</button>
+  return (
+    <div className="container">
+      <div className="signin-signup">
+        <form action="" className="sign-in-form">
+          <h2 className="title">RECUPERAR CONTRASEÑA</h2>
+          <div className="input-field">
+            <i className="fas fa-user"></i>
+            <input type="email" placeholder="Correo Electronico" />
+          </div>
+          <input type="submit" value="Recuperar" className="btn" />
         </form>
       </div>
-      <div className="overlay-container">
-        <div className="overlay">
-          <div className="overlay-panel overlay-right">
-            <img className="logo" src={logo} alt="Logo" /> 
-            <p>SISTEMA DE GESTIÓN DE LA OFERTA ACADÉMICA Y SIMPLIFICACIÓN DE PROCESOS DE INSCRIPCIÓN</p>
-          </div>
+      <div className="panels-container">
+        <div className="panel right-panel">
+            <img className="image" src={logo} alt="Logo" />
+            <div className="content">
+                <p>SISTEMA DE GESTIÓN DE LA OFERTA ACADÉMICA Y SIMPLIFICACIÓN DE PROCESOS DE INSCRIPCIÓN</p>
+            </div>
         </div>
       </div>
+
     </div>
   );
 };
