4aab7d27c4a6509fbf2b0ada0841b5b1905c67e1
Wilmer Rodolfo Castro Ladino <wilmer.castro@unillanos.edu.co>
2024-01-23 12:17:03 -0500

    Falta agregar responsive

--- src/pages/StudentsForCourse/StudentsForCourse.jsx
+++ src/pages/StudentsForCourse/StudentsForCourse.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Swal from 'sweetalert2';
-import { Students } from './StudentsForCourseStyle';
+import { Students, ButtonChange } from './StudentsForCourseStyle';
 import { getDataEnrollment, updateDataEnrollment, getDataStudentsInfo } from '../../store/enrollment/enrollmentActions';
 import { getStudents, setStudents } from '../../store/enrollment/enrollmentSlice';
 
@@ -13,7 +13,14 @@ export default function StudentsForCourse() {
   const { students, studentsInfo } = useSelector((store) => store.enrollment);
   const [stateStudent, setStateStudent] = useState([]);
   const [nameStudent, setNameStudent] = useState([]);
-
+  const states = [
+    "Pendiente",
+    "Aprobado",
+    "Rechazado",
+    "Por revisar",
+    "Revisado",
+    "Certificado",
+  ];
   useEffect(() => {
     const hiddenDateStudents = async () => {
       try {
@@ -45,81 +52,90 @@ export default function StudentsForCourse() {
     setStateStudent(stateStudentNew);
   };
 
-  const handleGuardarAccion = async (id, index) => {
-    const result = await Swal.fire({
-      title: 'Confirmar',
-      text: '¿Estás seguro de que deseas cambiar el estado?',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Sí, guardar',
-      cancelButtonText: 'Cancelar',
-    });
-    if (result.isConfirmed) {
-      const enrollmentToUpdate = students.find(enrollment => enrollment.id === id);
-      if (enrollmentToUpdate) {
-        const updatedEnrollment = { ...enrollmentToUpdate, state: stateStudent[index] };
-        dispatch(updateDataEnrollment(updatedEnrollment));
-        Swal.fire({
-          icon: 'success',
-          title: 'Actualización exitosa',
-          text: 'La información se ha actualizado correctamente.',
-        });
-        const updatedStudents = students.map((student) =>
-          student.id === id ? { ...student, state: stateStudent[index] } : student
-        );
-        dispatch(setStudents(updatedStudents));
-      } else {
-        Swal.fire({
-          icon: 'error',
-          title: 'Error',
-          text: 'No se encontró la información para actualizar.',
-        });
+  const handleGuardarAccion = async (id, index, stateAct) => {
+    if(stateAct === stateStudent[index]){
+      Swal.fire({
+        icon: 'info',
+        title: 'Estado igual',
+        text: 'El estado seleccionado es el mismo que el actual.',
+      });
+    } else {
+      const result = await Swal.fire({
+        title: 'Confirmar',
+        text: '¿Estás seguro de que deseas cambiar el estado?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, guardar',
+        cancelButtonText: 'Cancelar',
+      });
+      if (result.isConfirmed) {
+        const enrollmentToUpdate = students.find(enrollment => enrollment.id === id);
+        if (enrollmentToUpdate) {
+          const updatedEnrollment = { ...enrollmentToUpdate, state: stateStudent[index] };
+          dispatch(updateDataEnrollment(updatedEnrollment));
+          Swal.fire({
+            icon: 'success',
+            title: 'Actualización exitosa',
+            text: 'La información se ha actualizado correctamente.',
+          });
+          const updatedStudents = students.map((student) =>
+            student.id === id ? { ...student, state: stateStudent[index] } : student
+          );
+          dispatch(setStudents(updatedStudents));
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'No se encontró la información para actualizar.',
+          });
+        }
       }
     }
   };
 
-  if (!students || !studentsInfo) {
-    return <div>Cargando...</div>;
+  if (!students || !studentsInfo || students.length === 0 || studentsInfo.length === 0) {
+    return <div>No hay estudiantes para mostrar.</div>;
   }  
 
   return (
     <Students>
       <h2>Tabla de Estudiantes</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Id de Estudiantes</th>
-            <th>Nombre Completo</th>
-            <th>Estado de la Solicitud</th>
-            <th colSpan="2">Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {students.map((estudiante, index) => (
-            <tr key={index}>
-              <td>{estudiante.Id_student}</td>
-              <td>{nameStudent[index]}</td>
-              <td>{estudiante.state}</td>
-              <td>
-                <select
-                  value={stateStudent[index]}
-                  onChange={(e) => handleChangeEstado(index, e.target.value)}
-                >
-                  <option value="Pendiente">Pendiente</option>
-                  <option value="Aprobado">Aprobado</option>
-                  <option value="Rechazado">Rechazado</option>
-                  <option value="Por revisar">Por Revisar</option>
-                  <option value="Revisado">Revisado</option>
-                  <option value="Certificado">Certificado</option>
-                </select>
-              </td>
-              <td>
-                <button onClick={() => handleGuardarAccion(estudiante.id, index)}>Guardar</button>
-              </td>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Id de Estudiantes</th>
+              <th>Nombre Completo</th>
+              <th>Estado de la Solicitud</th>
+              <th colSpan="2">Acciones</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {students.map((estudiante, index) => (
+              <tr key={index}>
+                <td>{estudiante.Id_student}</td>
+                <td>{nameStudent[index]}</td>
+                <td>{estudiante.state}</td>
+                <td>
+                  <select
+                    value={stateStudent[index]}
+                    onChange={(e) => handleChangeEstado(index, e.target.value)}
+                  >
+                    {
+                      states.map((state, index)=>(
+                        <option key={`${index}${state}`} value={state}>{state}</option>
+                      ))
+                    }
+                  </select>
+                </td>
+                <td>
+                  <ButtonChange onClick={() => handleGuardarAccion(estudiante.id, index, estudiante.state)}>Cambiar</ButtonChange>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Students>
   );
 }
--- src/pages/StudentsForCourse/StudentsForCourseStyle.jsx
+++ src/pages/StudentsForCourse/StudentsForCourseStyle.jsx
@@ -7,13 +7,47 @@ export const Students = styled.div`
     h2{
         font-weight: 900;
     }
-    table{
-        width: 100%;
-        th{
-            padding: 1%;
-        }
-        td {
-            padding: 1%;
+    div{
+        width: auto;
+        overflow-x: auto;
+        table{
+            width: 100%;
+            font-size: 100%;
+            th{
+                padding: 2%;
+            }
+            td {
+                padding: 2%;
+                width: 100%;
+                text-align: justify;
+                white-space: nowrap;
+                select{
+                    font-size: 100%;
+                    font-weight: 500;
+                }
+            }
         }
     }
+    @media (max-width: 768px) {
+      /* Estilos específicos para pantallas más pequeñas */
+      /* Puedes ajustar según tus necesidades */
+      padding: 2%;
+      font-size: 80%;
+   }
 `
+
+export const ButtonChange = styled.button`
+    font-family: 'DM Sans', sans-serif;
+    font-size: 100%;
+    color: #fff;
+    background-color: #24753a;
+    border: 2px solid #24753a;
+    border-radius: 5px;
+    padding: 0.5rem;
+    &:hover{
+        cursor: pointer;
+        background-color: #fff;
+        color: #24753a;
+        transition: background-color 0.3s ease;
+    }
+`;
